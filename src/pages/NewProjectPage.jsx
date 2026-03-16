b0cd551e9493bd4071e5818f7016f6b5e3234474
mbailo <mbailo.c@gmail.com>
2026-03-16 21:37:41 +0100

    Update NewProjectPage.jsx

--- src/pages/NewProjectPage.jsx
+++ src/pages/NewProjectPage.jsx
@@ -1,1 +1,8 @@
-
+export default function NewProjectPage() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1>Alta de Proyecto</h1>
+      <p>Aquí irá el formulario actual.</p>
+    </div>
+  );
+}
