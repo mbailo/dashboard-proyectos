8fa360fb3d570f961ce432082a5a118ced8f65b2
mbailo <mbailo.c@gmail.com>
2026-03-16 22:24:36 +0100

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 export default function DashboardPage() {
   return (
-    <div style={{ padding: "24px" }}>
-      <h1>Dashboard de Proyectos</h1>
+    <div>
+      <h2>Dashboard de Proyectos</h2>
       <p>Pantalla principal en construcción.</p>
     </div>
   );
