9743fdb4086d3a59e3823e5eb0d6e2dc713cfe5b
mbailo <mbailo.c@gmail.com>
2026-03-16 21:39:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import NewProjectPage from "./pages/NewProjectPage";
+import ProjectDetailPage from "./pages/ProjectDetailPage";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/proyectos/nuevo" element={<NewProjectPage />} />
+        <Route path="/proyectos/:id" element={<ProjectDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
