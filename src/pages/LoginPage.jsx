c0a6c4cea47f48eee4f5bae5a8fb58e571ca4f4d
mbailo <mbailo.c@gmail.com>
2026-03-18 21:17:02 +0100

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabase";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
