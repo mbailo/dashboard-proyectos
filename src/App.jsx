e3fc3839d0351438a505f6058c4745efbecb2071
mbailo <mbailo.c@gmail.com>
2026-03-18 21:44:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,21 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import DashboardPage from "./pages/DashboardPage";
 import NewProjectPage from "./pages/NewProjectPage";
 import ProjectDetailPage from "./pages/ProjectDetailPage";
 import LoginPage from "./pages/LoginPage";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <DashboardPage />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/nuevo-proyecto"
-          element={
-            <ProtectedRoute>
-              <NewProjectPage />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/proyecto/:id"
-          element={
-            <ProtectedRoute>
-              <ProjectDetailPage />
-            </ProtectedRoute>
-          }
-        />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/proyectos/nuevo" element={<NewProjectPage />} />
+          <Route path="/proyectos/:id" element={<ProjectDetailPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
