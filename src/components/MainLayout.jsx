9b7caa6beed4a7f67513877523efd57841a61717
mbailo <mbailo.c@gmail.com>
2026-03-20 23:20:17 +0100

    Update MainLayout.jsx

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -26,7 +26,7 @@ export default function MainLayout() {
       >
         <div
           style={{
-            maxWidth: "1800px",
+            maxWidth: "1500px",
             margin: "0 auto",
             display: "flex",
             alignItems: "center",
@@ -53,7 +53,7 @@ export default function MainLayout() {
         </div>
       </header>
 
-      <main style={{ maxWidth: "1800px", margin: "0 auto", padding: "24px 18px" }}>
+      <main style={{ maxWidth: "1500px", margin: "0 auto", padding: "24px 18px" }}>
         <Outlet />
       </main>
     </div>
